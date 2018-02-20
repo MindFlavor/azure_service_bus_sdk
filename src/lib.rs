5f6708f7023506faaef0fab02d4ad5dbe684c74d
Francesco Cogno <francesco.cogno@outlook.com>
2018-02-20 15:57:43 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+#![feature(conservative_impl_trait)]
+
+extern crate azure_core_sdk;
+extern crate base64;
+
+#[macro_use]
+extern crate hyper;
+extern crate mime;
+
+extern crate time;
+
+extern crate chrono;
+extern crate crypto;
+
+extern crate futures;
+extern crate hyper_tls;
+extern crate native_tls;
+extern crate tokio_core;
+
+#[macro_use]
+extern crate log;
+
+extern crate url;
+
+pub mod event_hub;
